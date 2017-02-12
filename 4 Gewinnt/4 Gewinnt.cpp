a33ab0115a8562195f603fb7f5b829f00a59172e
Lars <elyday@posteo.de>
2017-02-12 18:49:33 +0100

    Initialize

---
+++ 4 Gewinnt/4 Gewinnt.cpp
@@ -0,0 +1,56 @@
+/*
+Author: Lars Riﬂe <l.risse@tevaris.de>
+*/
+
+#include "stdafx.h"
+#include <iostream>
+
+// Customs Includes
+#include "Functions.h"
+using namespace std;
+ 
+int main()
+{
+	int fieldArray[8][8];
+	int winplayer = 0, actor = 1;
+	
+	startNewGame(fieldArray);
+	
+	while (winplayer == 0) {
+		int column = 100, field = -1;
+		Ausgabe(fieldArray);
+
+		while (column == 100 || field == -1) {
+			column = Eingabe(actor);
+			if (column != 100) {
+				field = nextFree(fieldArray, column);
+			}
+		}
+
+		Zug(fieldArray, column, field, actor);
+		int win = checkHorizontal(fieldArray, column - 1, actor);
+		if (win == 1) {
+			winplayer = actor;
+		}
+		else {
+			int win = checkPerpendicular(fieldArray, column - 1, actor);
+			if (win == 1)
+				winplayer = actor;
+		}
+
+		if (actor == 1)
+			actor = 2;
+		else if (actor == 2)
+			actor = 1;
+		else
+			actor = 1;
+
+		system("cls");
+	}
+	
+	Ausgabe(fieldArray);
+	cout << "Spieler " << winplayer << " hat gewonnen.\n";
+
+	system("pause");
+    return 0;
+}
---
+++ 4 Gewinnt/Functions.h
@@ -0,0 +1,76 @@
+/*
+Author: Lars Riﬂe <l.risse@tevaris.de>
+*/
+#pragma once
+using namespace std;
+
+void startNewGame(int fieldArray[8][8]) {
+	for (int i = 0; i < 8; i++)
+	{
+		for (int d = 0; d < 8; d++)
+		{
+			fieldArray[i][d] = 0;
+		}
+	}
+}
+
+void Ausgabe(int fieldArray[8][8]) {
+	for (int i = 0; i < 8; i++)
+	{
+		for (int d = 0; d < 8; d++)
+		{
+			cout << fieldArray[i][d] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+void Zug(int fieldArray[8][8], int column, int field, int actor) {
+	fieldArray[field][column - 1] = actor;
+}
+
+int nextFree(int fieldArray[8][8], int column) {
+	for (int i = 7; i > -1; i--) {
+		if (fieldArray[i][column - 1] == 0) {
+			return i;
+		}
+	}
+	cout << "Die Spalte ist bereits voll, w‰hle eine andere!\n";
+	return -1;
+}
+
+int Eingabe(int actor) {
+	int column;
+	if (actor == 1) {
+		cout << "Spieler 1: W‰hle eine Spalte (1-8): ";
+		cin >> column;
+	}
+	else if (actor == 2) {
+		cout << "Spieler 2: W‰hle eine Spalte (1-8): ";
+		cin >> column;
+	}
+
+	if (column >= 0 && column < 9) {
+		return column;
+	}
+	else {
+		cout << "Fehlerhafte Eingabe, wiederhole!\n";
+		return 100;
+	}
+}
+
+int checkHorizontal(int fieldArray[8][8], int column, int actor) {
+	for (int i = 7; i > -1; i--) {
+		if ((fieldArray[i][column] == actor && fieldArray[i - 1][column] == actor && fieldArray[i - 2][column] == actor && fieldArray[i - 3][column] == actor) || (fieldArray[i][column] == actor && fieldArray[i + 1][column] == actor && fieldArray[i + 2][column] == actor && fieldArray[i + 3][column] == actor) || (fieldArray[i][column] == actor && fieldArray[i + 1][column] == actor && fieldArray[i - 1][column] == actor && fieldArray[i - 2][column] == actor) || (fieldArray[i][column] == actor && fieldArray[i + 1][column] == actor && fieldArray[i - 1][column] == actor && fieldArray[i + 2][column] == actor)) {
+			return 1;
+		}
+	}
+}
+
+int checkPerpendicular(int fieldArray[8][8], int column, int actor) {
+	for (int i = 7; i > -1; i--) {
+		if ((fieldArray[i][column] == actor && fieldArray[i][column - 1] == actor && fieldArray[i][column - 2] == actor && fieldArray[i][column - 3] == actor) || (fieldArray[i][column] == actor && fieldArray[i][column + 1] == actor && fieldArray[i][column + 2] == actor && fieldArray[i][column + 3] == actor) || (fieldArray[i][column] == actor && fieldArray[i][column + 1] == actor && fieldArray[i][column - 1] == actor && fieldArray[i][column - 2] == actor) || (fieldArray[i][column] == actor && fieldArray[i][column + 1] == actor && fieldArray[i][column - 1] == actor && fieldArray[i][column + 2] == actor)) {
+			return 1;
+		}
+	}
+}
