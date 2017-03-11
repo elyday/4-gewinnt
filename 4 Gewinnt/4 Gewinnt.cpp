724076ca649bd9a7e8ae5fa1804939eb3e6153e3
larsr <l.risse@tevaris.de>
2017-03-11 23:01:09 +0100

    Refactored and added pointer

--- 4 Gewinnt/4 Gewinnt.cpp
+++ 4 Gewinnt/4 Gewinnt.cpp
@@ -4,61 +4,72 @@ Author: Lars Riﬂe <l.risse@tevaris.de>
 
 #include "stdafx.h"
 #include <iostream>
+using namespace std;
 
 // Customs Includes
 #include "Functions.h"
-using namespace std;
  
 int main()
 {
 	int fieldArray[8][8];
 	int winplayer = 0, actor = 1;
+	bool boolean = true;
 	
 	startNewGame(fieldArray);
 	
-	while (winplayer == 0) {
-		int column = 100, field = -1;
-		Ausgabe(fieldArray);
+	while (boolean) {
+		while (winplayer == 0) {
+			int column = 100, field = -1;
+			Ausgabe(fieldArray);
 
-		while (column == 100 || field == -1) {
-			column = Eingabe(actor);
-			if (column == 101) {
-				field = 101;
-			} else if (column != 100) {
-				field = nextFree(fieldArray, column);
+			while (column == 100 || field == -1) {
+				column = Eingabe(&actor);
+				if (column == 101) {
+					field = 101;
+				}
+				else if (column == 102) {
+					field = 102;
+				}
+				else if (column != 100) {
+					field = nextFree(fieldArray, &column);
+				}
 			}
-		}
 
-		if (field < 8 && field >= 0) {
-			Zug(fieldArray, column, field, actor);
-			int win = checkHorizontal(fieldArray, column - 1, actor);
-			if (win == 1) {
+			if (field < 8 && field >= 0) {
+				Zug(fieldArray, &column, &field, &actor);
+				int win = checkHorizontal(fieldArray, &column - 1, &actor);
+				if (win == 1) {
+					winplayer = actor;
+				}
+				else {
+					int win = checkPerpendicular(fieldArray, &column - 1, &actor);
+					if (win == 1)
+						winplayer = actor;
+				}
+			}
+
+			if (actor == 1)
+				actor = 2;
+			else if (actor == 2)
+				actor = 1;
+			else
+				actor = 1;
+
+			if (field == 101) {
 				winplayer = actor;
 			}
-			else {
-				int win = checkPerpendicular(fieldArray, column - 1, actor);
-				if (win == 1)
-					winplayer = actor;
+			else if (field == 102) {
+				boolean = false;
+				break;
 			}
+
+			system("cls");
 		}
 
-		if (actor == 1)
-			actor = 2;
-		else if (actor == 2)
-			actor = 1;
-		else
-			actor = 1;
-
-		if (field == 101) { 
-			winplayer = actor;
+		if (winplayer == 1 || winplayer == 2) {
+			cout << "Spieler " << winplayer << " hat gewonnen.\n";
+			resetGame(fieldArray, &winplayer, &actor);
 		}
-
-		system("cls");
-	}
-	
-	if (winplayer == 1 || winplayer == 2) {
-		Ausgabe(fieldArray);
-		cout << "Spieler " << winplayer << " hat gewonnen.\n";
 	}
 
 	system("pause");
